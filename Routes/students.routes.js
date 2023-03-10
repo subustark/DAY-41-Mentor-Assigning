c73dbd5ae451917ab4f58c90463e8abe41c1fa47
subustark <subustark25@gmail.com>
2023-03-10 09:43:23 +0530

    first commit

---
+++ Routes/students.routes.js
@@ -0,0 +1,54 @@
+const router=require("express").Router()
+
+const mongo=require("../shared/mongo.js")
+
+const{ObjectId} = require("mongodb")
+
+const {studentSchema} =require("../shared/schema")
+
+
+router.get("/",async (req,res)=>{
+    let data= await mongo.students.find().toArray()
+    console.log(data)
+    res.send(data)
+})
+
+
+router.post("/",async(req,res)=>{
+const {value,error}= studentSchema.validate(req.body)
+if(error) return res.status(400).send({Error:error.details[0].message})
+let data;
+if(value.mentorId === "unassigned")  
+{ data =await mongo.students.insert(value)
+}
+else {
+    data =await mongo.students.insert({...value,mentorId:ObjectId(value.mentorId)})
+}
+res.send(value)
+})
+
+router.put("/assignMentor",async(req,res)=>{
+    console.log("in assignMentor")
+
+    let data=await mongo.students.updateMany({name:{$in:req.body.names}},{$set:{mentorId:ObjectId(req.body.mentorId)}})
+    res.status(201).send("updated")
+})
+
+router.put("/:_id",async(req,res)=>{
+    let id=req.params._id
+   console.log(id,req.body) 
+   let data=await mongo.students.findOneAndUpdate({_id:ObjectId(id)},
+                                            {$set:{mentorId:ObjectId(req.body.mentorId)}},
+                                            {returnNewDocument:true})
+    res.send(data)
+})
+
+router.get("/getMentor/:mentorId",async(req,res)=>{
+    let id=req.params.mentorId
+    console.log(id)
+    let data= await mongo.students.find({mentorId:ObjectId(id)}).toArray()
+    res.send(data)
+})
+
+
+module.exports=router
---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const cors=require("cors")
+const express = require("express");
+const app=express();
+const mongo= require("./shared/mongo");
+const studentRoutes=require("./Routes/students.routes");
+const mentorRoutes=require("./Routes/mentors.routes");
+
+
+app.use(cors());
+(async()=>{
+
+try 
+{
+   
+
+    await mongo.connect()
+
+    app.use(express.json())
+
+    app.get('/', function(req, res, next) {
+        res.send('Server running successfully🎉🎉');
+      });
+
+app.use("/students",studentRoutes)
+
+app.use("/mentors",mentorRoutes)
+
+const port = process.env.PORT || 3001
+
+app.listen(port,()=>{
+    console.log("server is running on ",port)
+})
+
+}
+catch(err)
+{console.log("ERROR in Connecting to DB",err)}
+})();
